29570fe0f72a9a294908f0f399b10603cee8e07e
Emilio Coronado <emilio.coronado@gmail.com>
2021-07-27 19:33:27 +0900

    Fix

--- hello-dags.py
+++ hello-dags.py
@@ -2,10 +2,10 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
-from airflow.operators import MultiplyBy5Operator
+#from airflow.operators import MultiplyBy5Operator
 
 def print_hello():
- return 'Hello Wolrd'
+ return 'Hello Dag !!'
 
 dag = DAG('hello_world', description='Hello world example', schedule_interval='0 12 * * *', start_date=datetime(2017, 3, 20), catchup=False)
 
@@ -13,9 +13,9 @@ dummy_operator = DummyOperator(task_id='dummy_task', retries = 3, dag=dag)
 
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
 
-multiplyby5_operator = MultiplyBy5Operator(my_operator_param='my_operator_param',
-                                task_id='multiplyby5_task', dag=dag)
+#multiplyby5_operator = MultiplyBy5Operator(my_operator_param='my_operator_param',
+#                                task_id='multiplyby5_task', dag=dag)
 
 dummy_operator >> hello_operator
 
-dummy_operator >> multiplyby5_operator
+#dummy_operator >> multiplyby5_operator
